ba25ebe4bd1f56edf8c804508f49511b228b5f95
anthony-jourdon <jourdon.anthon@gmail.com>
2026-02-17 17:51:55 +0100

    Add the use of a yaml file for postprocessing yin yang 3D models

--- examples/postproc_3d_yinyang.py
+++ examples/postproc_3d_yinyang.py
@@ -38,8 +38,6 @@ class IOutils:
       print(f"Created output directory: {self.output_dir}")
 
     self.is_surface:bool = kwargs.get("is_surface", True)
-    if self.is_surface:
-      self.prefix += "-surface"
 
     # stepping parameters for time series processing
     self.step:int|None  = kwargs.get("step", None)
@@ -67,6 +65,22 @@ class IOutils:
       "pressure": "vp", # pressure is stored in the 4th component of the velocity file
     }
     return
+  
+  def __str__(self) -> str:
+    s  = f"Model: {self.model}\n"
+    s += f"Model directory: {self.model_dir}\n"
+    s += f"Output directory: {self.output_dir}\n"
+    s += f"Output fields: {self.output_fields}\n"
+    s += f"PVD file: {self.pvd}\n"
+    s += f"Regions: {self.regions}\n"
+    s += f"Reset: {self.reset_fields}\n"
+    s += f"Prefix: {self.prefix}\n"
+    s += f"Step: {self.step}\n"
+    s += f"Step start: {self.step_start}\n"
+    s += f"Step end: {self.step_end}\n"
+    s += f"Delta step: {self.steps_idx[1] - self.steps_idx[0] if self.steps_idx is not None else 1}\n"
+    s += f"Surface: {self.is_surface}\n"
+    return s
 
 class Field:
   def __init__(self, name:str, io_utils:IOutils, mesh:spv.YinYangMesh):
@@ -341,12 +355,8 @@ def write_step(mesh:spv.YinYangMesh, io_utils:IOutils, class_fields:dict[str,Fie
   return
 
 def clean_up(class_instances:dict[str,Field]) -> None:
-  print("\tCleaning up field data from mesh...")
-  t0 = perf_counter()
   for field in class_instances:
     class_instances[field].reset()
-  t1 = perf_counter()
-  print(f"\tField data cleaned up in {t1-t0:g} seconds.")
   return
 
 def process_full_directory(mesh:spv.YinYangMesh, io_utils:IOutils, class_fields:dict[str,Field]) -> None:
@@ -437,20 +447,152 @@ def process_model(iou:IOutils) -> None:
   return
 
 def main():
+  description = "Postprocessing parser for StagYY 3D YinYang models"
 
-  return
+  parser = argparse.ArgumentParser(
+    prog="postproc_3d_yinyang.py", 
+    description=description, 
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter
+  )
+  parser.add_argument(
+    '-f',
+    '--file',
+    type=str,
+    help='Path to the yaml input file',
+    dest='yaml_file',
+    required=True
+  )
+  args = parser.parse_args()
+  if not os.path.exists(args.yaml_file):
+    help_str = f"YAML file {args.yaml_file} not found.\n"
+    raise FileNotFoundError(help_str)
+
+  with open(args.yaml_file) as f:
+    user:dict = yaml.load(f, Loader=yaml.FullLoader)
+  #####################
+  # Steps information #
+  #####################
+  if "steps" not in user:
+    user["steps"] = {
+      "step": None,
+      "step_start": 0,
+      "step_end": None,
+      "dstep": 1,
+    }
+    help_str = f"steps options not provided in yaml file, defaulting to:\n{user['steps']}\n"
+    help_str += f"Consider adding a steps section to your yaml file to specify the steps to process\n"
+    print(help_str)
+
+  #####################
+  # Paths information #
+  #####################
+  if "paths" not in user:
+    help_str  = f"paths section not found in yaml file.\n"
+    help_str += f"Use the paths section to specify the paths to the model directory and output directory using the following keys:\n"
+    help_str += f"  model: path to the model directory\n"
+    help_str += f"  directory: path to the output directory\n"
+    help_str += f"  base:\n"
+    help_str += f"    model: absolute path to the base model directory\n"
+    help_str += f"    output: absolute path to the base output directory\n"
+    raise ValueError(help_str)
+  
+  paths:dict = user["paths"]
+  if "model" not in paths:
+    help_str = f"model key not found in paths section of yaml file.\n"
+    help_str += f"Use the model key to specify the name of the model.\n"
+    raise ValueError(help_str)
+  if "directory" not in paths:
+    help_str = f"directory key not found in paths section of yaml file.\n"
+    help_str += f"Use the directory key to specify the name of the model directory where the output files are located.\n"
+    raise ValueError(help_str)
+  if "base" not in paths:
+    help_str = f"base key not found in paths section of yaml file.\n"
+    help_str += f"Use the base key to specify the absolute paths to the base model and output directories using the following keys:\n"
+    help_str += f"  model: absolute path to the base model directory\n"
+    help_str += f"  output: absolute path to the base output directory\n"
+    raise ValueError(help_str)
+  if "model" not in paths["base"]:
+    help_str = f"model key not found in base section of paths section of yaml file.\n"
+    help_str += f"Use the model key to specify the absolute path to the base model directory.\n"
+    raise ValueError(help_str)
+  if "output" not in paths["base"]:
+    help_str = f"output key not found in base section of paths section of yaml file.\n"
+    help_str += f"Use the output key to specify the absolute path to the base output directory.\n"
+    raise ValueError(help_str)
+
+  ######################
+  # Fields information #
+  ######################
+  if "fields" not in user:
+    help_str  = f"fields section not found in yaml file.\n"
+    help_str += f"Use the fields section to specify the fields to be added to the output mesh using the following key:\n"
+    help_str += f"  process: list of fields to be added to the output mesh.\n"
+    help_str += f"  regions: list of region fields to be merged into a single regions field in the output mesh.\n"
+    help_str += f"  surface: boolean indicating whether to output surface fields (True) or volume fields (False), default: False.\n"
+    raise ValueError(help_str)
+  
+  fields:dict = user["fields"]
+  if "process" not in fields:
+    help_str = f"process key not found in fields section of yaml file.\n"
+    help_str += f"Use the process key to specify the list of fields to be added to the output mesh.\n"
+    raise ValueError(help_str)
+  
+  prefix = paths.get("prefix", "")
+  is_surface = fields.get("surface", False)
+  if is_surface:
+    prefix += "-surface"
+  default_pvd = f"timeseries_{paths['model']}{prefix}.pvd"
+  pvd = paths.get("pvd", default_pvd)
+
+  output_fields:list[str] = fields["process"]
+  regions_list  = fields.get("regions", ["composition"])
+  for region in regions_list:
+    if region in fields["process"]:
+      output_fields.append("regions")
+      break
+
+  step       = user["steps"].get("step", None)
+  reset      = user["steps"].get("reset", False)
+  output_dir = os.path.join(paths["base"]["output"],paths["directory"])
+  start_step = user["steps"].get("start", 0)
+  if step is None:
+    # check if a pvd file already exists
+    if os.path.exists(os.path.join(output_dir,pvd)) and not reset:
+      print(f"Found exisiting pvd file {pvd}, appending new time steps.")
+      timeseries = spv.timeseries_process(os.path.join(output_dir,pvd))
+      start_step = int(timeseries["step"][-1]) + 1
+      print(f"Updated start step to {start_step} based on existing pvd file.")
+
+  io_utils = IOutils(
+    model_name=paths["model"],
+    model_dir=paths["directory"],
+    basedir=paths["base"]["model"],
+    pvd=pvd,
+    output_dir=output_dir,
+    output_fields=output_fields,
+    regions=regions_list,
+    step=step,
+    step_start=start_step,
+    step_end=user["steps"].get("end", None),
+    dstep=user["steps"].get("delta", 1),
+    is_surface=is_surface,
+    reset_fields=reset,
+    prefix=prefix,
+  )
+  process_model(io_utils)
+  exit(0)
 
 def test():
   model = "llsvp"
   mdir  = "LLSVP-Run4"
-  pvd_fname = f"timeseries_{model}_surface.pvd"
+  pvd_fname = f"timeseries_{model}-surface.pvd"
   basedir = os.path.join("/data","ens","ncoltice","CLAUDIO")
   output_dir = os.path.join("/data","jourdon","llsvp-pandora-vtu","surface-yy",mdir)
   output_fields = ["velocity", "velocity_r", "e2", "temperature", "divergence", "composition", "primordial", "proterozoic"]
   reset = False
   step  = None
   start_step = 0
-  end_step = 30
+  end_step = None
   # will be given by user in real use case
   regions = ["primordial", "composition", "proterozoic"]
   for region in regions:
@@ -463,7 +605,6 @@ def test():
     if os.path.exists(os.path.join(output_dir,pvd_fname)) and not reset:
       print(f"Found exisiting pvd file {pvd_fname}, appending new time steps.")
       timeseries = spv.timeseries_process(os.path.join(output_dir,pvd_fname))
-      print(timeseries)
       start_step = int(timeseries["step"][-1]) + 1
       print(f"Updated start step to {start_step} based on existing pvd file.")
 
@@ -559,4 +700,4 @@ def test2():
   return
 
 if __name__ == "__main__":
-  test()
+  main()
