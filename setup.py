95d312f7392b07a235dc61e8450d7c1f1794babf
anthony-jourdon <jourdon.anthon@gmail.com>
2026-03-06 16:15:18 +0100

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+  name="stagpyviz",
+  version="1.0.0",
+  packages=find_packages(include=["stagpyviz", "stagpyviz.*"]),
+  install_requires=[
+    "numpy",
+    "scipy",
+    "pyvista"
+  ],
+  author="Anthony Jourdon",
+  description="A Python package for visualizing StagYY Yin-Yang models",
+  long_description=open("README.md").read(),
+  long_description_content_type="text/markdown",
+  url="https://github.com/anthony-jourdon/stagpyviz",
+  classifiers=[
+    "Programming Language :: Python :: 3"
+  ],
+  python_requires=">=3.13"
+)
