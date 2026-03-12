497369e05da29a344434d2b4c90a616cb2315788
anthony-jourdon <jourdon.anthon@gmail.com>
2026-03-12 09:52:48 +0100

    Add volume computing for wedge elements

--- stagpyviz/elements/wedge_3d.py
+++ stagpyviz/elements/wedge_3d.py
@@ -1,4 +1,5 @@
 import numpy as np
+from time import perf_counter
 
 # Use relative import when imported as module, absolute when run directly
 try:
@@ -136,3 +137,108 @@ class Wedge3D(Element3D):
     xi  = np.array([1.0/3.0, 1.0/3.0, 0.0], dtype=np.float64)
     GNi = self.evaluate_GNi(xi)
     return GNi
+  
+  def quadrature_rule_3x2(self):
+    one_sixth = np.float64(0.16666666666666666)
+    two_third = np.float64(0.6666666666666666)
+    one_sqrt3 = np.float64(0.5773502691896258)
+
+    weights = np.full(6, 1.0/6.0, dtype=np.float64)
+    points = np.array([
+      [one_sixth, one_sixth, -one_sqrt3],
+      [two_third, one_sixth, -one_sqrt3],
+      [one_sixth, two_third, -one_sqrt3],
+      [one_sixth, one_sixth, one_sqrt3],
+      [two_third, one_sixth, one_sqrt3],
+      [one_sixth, two_third, one_sqrt3]
+    ], dtype=np.float64)
+    return weights, points
+
+  def compute_volume_1pt_rule(self, xe:np.ndarray) -> float|np.ndarray:
+    """
+    Compute the volume of the wedge element given the coordinates of its vertices 
+    using a 1 quadrature point rule.
+    The volume is computed as:
+
+    .. math::
+      V = |det(\\boldsymbol{J})|
+
+    where :math:`\\boldsymbol{J}` is the Jacobian matrix of the transformation from the reference element 
+    to the physical element evaluated at the element centroid.
+    :param numpy.ndarray xe: 
+      For a single element: array of shape ``(6, 3)`` containing the coordinates of the wedge vertices.
+      For multiple elements: array of shape ``(number_of_cells, 6, 3)`` containing the coordinates of the wedge vertices for each element.
+    :return: Volume of the element(s).
+    :rtype: float|np.ndarray
+    """
+    t0 = perf_counter()
+    GNi  = self.GNi_centroid()
+    J    = self.evaluate_Jacobian(GNi, xe)
+    detJ = self.evaluate_detJ(J)
+    t1 = perf_counter()
+    print(f"Computed the volume of {detJ.shape[0]} cells in {t1-t0:g} seconds using the 1 quadrature point rule")
+    return np.abs(detJ)
+  
+  def compute_volume_3x2pt_rule(self, xe:np.ndarray) -> float|np.ndarray:
+    """
+    Compute the volume of the wedge element given the coordinates of its vertices 
+    using a 3x2 quadrature points rule.
+    The volume is computed as:
+
+    .. math::
+      V = \\sum_q |det(\\boldsymbol{J}_q)| w_q
+
+    where :math:`\\boldsymbol{J}` is the Jacobian matrix of the transformation from the reference element 
+    to the physical element evaluated at the element centroid.
+    :param numpy.ndarray xe: 
+      For a single element: array of shape ``(6, 3)`` containing the coordinates of the wedge vertices.
+      For multiple elements: array of shape ``(number_of_cells, 6, 3)`` containing the coordinates of the wedge vertices for each element.
+    :return: Volume of the element(s).
+    :rtype: float|np.ndarray
+    """
+    t0 = perf_counter()
+    w,qp = self.quadrature_rule_3x2()
+    nqp  = w.shape[0]
+    if xe.ndim == 2:
+      ncells = 1
+      volume = 0.0
+    elif xe.ndim == 3:
+      ncells = xe.shape[0]
+      volume = np.zeros((ncells), dtype=np.float64)
+    else:
+      raise ValueError(f"Input coordinates array xe must have shape (6, 3) for a single element or (number_of_cells, 6, 3) for multiple elements, got {xe.shape}")
+    for q in range(nqp):
+      GNi  = self.evaluate_GNi(qp[q])
+      J    = self.evaluate_Jacobian(GNi, xe)
+      detJ = self.evaluate_detJ(J)
+      volume += w[q] * np.abs(detJ)
+    t1 = perf_counter()
+    print(f"Computed the volume of {ncells} cells in {t1-t0:g} seconds using the 3x2 quadrature points rule")
+    return volume
+
+  def compute_volume(self,xe:np.ndarray,rule:str="1pt") -> float|np.ndarray:
+    """
+    Compute the volume of the wedge element given the coordinates of its vertices.
+    The volume can be computed using either a 1 quadrature point rule or a 3x2 quadrature points rule, 
+    specified by the ``rule`` parameter. See the methods 
+    :py:meth:`compute_volume_1pt_rule <stagpyviz.Wedge3D.compute_volume_1pt_rule>` and 
+    :py:meth:`compute_volume_3x2pt_rule <stagpyviz.Wedge3D.compute_volume_3x2pt_rule>` 
+    for details on the volume computation with each quadrature rule.
+
+    :param numpy.ndarray xe: 
+      For a single element: array of shape ``(6, 3)`` containing the coordinates of the wedge vertices.
+      For multiple elements: array of shape ``(number_of_cells, 6, 3)`` containing the coordinates of the wedge vertices for each element.
+    :param str rule: 
+      Quadrature rule to use for volume computation. 
+      Supported values are ``"1pt"`` for a 1 quadrature point rule and 
+      ``"3x2pt"`` for a 3x2 quadrature points rule. Default is ``"1pt"``.
+    :return: Volume of the element(s).
+    :rtype: float|np.ndarray
+    """
+    if rule == "1pt":
+      return self.compute_volume_1pt_rule(xe)
+    elif rule == "3x2pt":
+      return self.compute_volume_3x2pt_rule(xe)
+    else:
+      raise ValueError(f"Unknown quadrature rule '{rule}' for volume computation. Supported rules are '1pt' and '3x2pt'.")
+  
