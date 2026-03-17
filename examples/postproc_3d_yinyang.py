c56a42f79860887712d3c733f2fbe21391cd9687
anthony-jourdon <jourdon.anthon@gmail.com>
2026-03-17 08:45:58 +0100

    Changed the script to now call the package classes

--- examples/postproc_3d_yinyang.py
+++ examples/postproc_3d_yinyang.py
@@ -6,346 +6,7 @@ import pyvista as pvs
 import stagpyviz as spv
 from time import perf_counter
 
-
-class IOutils:
-  """
-  Class to manage the paths and file management for the postprocessing of StagYY 3D YinYang models.
-  This class is used to store the paths to the model directory and output directory, 
-  as well as the list of fields to be added to the output mesh.
-  This class also contains the stepping parameters for time series processing and the 
-  time series information to be written in the pvd file.
-
-  :param str model_name: Name of the model.
-  :param str model_dir: Name of the model directory.
-  :param str basedir: Absolute path to the base model directory.
-  :param str pvd: Name of the output pvd file.
-  :param str output_dir: Absolute path to the output directory.
-  :param list[str] output_fields: List of fields that can be retrieved from binary files output by StagYY.
-  :param list[str] regions: List of region fields to be merged into a single regions field in the output mesh, default: ["composition"].
-  :param bool is_surface: Boolean indicating whether to output the surface (True) or the volume (False), default: False.
-  :param int step: Step number to process, if None, the entire directory will be processed, default: None.
-  :param int step_start: Step number to start processing, default: 0.
-  :param int step_end: Step number to end processing, if None, the entire directory will be processed, default: None.
-  :param int dstep: Step increment for processing, default: 1.
-  :param bool reset_fields: Boolean indicating whether to reset the timeseries, default: False.
-  :param str prefix: Prefix to be added to the output file names, default: "".
-
-  Currently the supported fields that can be retrieved from binary files output by StagYY are:
-
-  .. code-block:: python
-
-    filelist = {
-      "composition": "c",
-      "divergence" : "div",
-      "e2"         : "ed",
-      "viscosity"  : "eta",
-      "nrc"        : "nrc", 
-      "primordial" : "prm",
-      "proterozoic": "prot", 
-      "stress"     : "str",
-      "temperature": "t",
-      "tracer"     : "tra",
-      "vorticity"  : "vor",
-      "velocity"   : "vp",
-      "pressure"   : "vp", # pressure is stored in the 4th component of the velocity file
-    }
-
-  """
-  def __init__(
-      self,
-      model_name:str, 
-      model_dir:str, 
-      basedir:str,
-      pvd:str,
-      output_dir:str,
-      output_fields:list[str],
-      **kwargs
-    ):
-    self.model:str               = model_name
-    self.mdir:str                = model_dir
-    self.basedir:str             = basedir
-    self.output_fields:list[str] = output_fields
-    self.pvd:str                 = pvd
-    self.prefix:str = kwargs.get("prefix", "")
-    # path to model directory
-    self.model_dir = os.path.join(basedir,model_dir)
-    self.time:float|None = None
-    self.timeseries:dict[str,list] = {
-      "time": [], # store time as string
-      "step": [], # store step number as string
-    }
-    # output directory
-    self.output_dir:str = output_dir
-    if not os.path.exists(self.output_dir):
-      os.makedirs(self.output_dir)
-      print(f"Created output directory: {self.output_dir}")
-
-    self.is_surface:bool = kwargs.get("is_surface", True)
-
-    # stepping parameters for time series processing
-    self.step:int|None  = kwargs.get("step", None)
-    self.step_start:int = kwargs.get("step_start", 0)
-    self.step_end:int   = kwargs.get("step_end", None)
-    dstep:int           = kwargs.get("dstep", 1)
-    self.steps_idx:np.ndarray|None = np.arange(self.step_start, self.step_end+1, dstep) if self.step_end is not None else None
-
-    self.regions:list[str] = kwargs.get("regions", ["composition"])
-    self.reset_fields:bool = kwargs.get("reset_fields", False)
-
-    self.filelist = {
-      "composition": "c",
-      "divergence": "div",
-      "e2" : "ed",
-      "viscosity": "eta",
-      "nrc": "nrc", # ??
-      "primordial": "prm",
-      "proterozoic": "prot", # ??
-      "stress" : "str",
-      "temperature": "t",
-      "tracer": "tra",
-      "vorticity": "vor",
-      "velocity": "vp",
-      "pressure": "vp", # pressure is stored in the 4th component of the velocity file
-    }
-    return
-  
-  def __str__(self) -> str:
-    s  = f"Model: {self.model}\n"
-    s += f"Model directory: {self.model_dir}\n"
-    s += f"Output directory: {self.output_dir}\n"
-    s += f"Output fields: {self.output_fields}\n"
-    s += f"PVD file: {self.pvd}\n"
-    s += f"Regions: {self.regions}\n"
-    s += f"Reset: {self.reset_fields}\n"
-    s += f"Prefix: {self.prefix}\n"
-    s += f"Step: {self.step}\n"
-    s += f"Step start: {self.step_start}\n"
-    s += f"Step end: {self.step_end}\n"
-    s += f"Delta step: {self.steps_idx[1] - self.steps_idx[0] if self.steps_idx is not None else 1}\n"
-    s += f"Surface: {self.is_surface}\n"
-    return s
-
-class Field:
-  """
-  Generic class to represent a field that can be added to the mesh. 
-  By default, this class assumes that the field is stored in a binray file output by StagYY.
-  Different types of fields can be created by inheriting from this class and implementing their own 
-  methods to retrieve the data and add them to the mesh.
-
-  :param str name: Name of the field, should correspond the the name registered in the IOutils class filelist attribute.
-  :param IOutils io_utils: Path and file management utilities
-  :param stagpyviz.YinYangMesh mesh: Volume mesh reconstructed as an unstructured grid
-  """
-  def __init__(self, name:str, io_utils:IOutils, mesh:spv.YinYangMesh):
-    self.name     = name
-    self.io_utils = io_utils
-    self.mesh     = mesh
-    self.values   = None
-    return
-  
-  def get_data(self) -> np.ndarray|None:
-    io_utils = self.io_utils
-    fname = f"{io_utils.model}_{io_utils.filelist[self.name]}{str(io_utils.step).zfill(5)}"
-    # check file existence
-    full_fname = os.path.join(io_utils.model_dir,fname)
-    if not os.path.exists(full_fname): 
-      print(f"\t\tFile {full_fname} not found, ignoring field {self.name}.")
-      return None
-    header, data = spv.read_stag_bin(full_fname)
-    self.io_utils.time = header["time"]
-    return data
-
-  def get_values(self) -> np.ndarray|None:
-    if self.values is not None:
-      return self.values
-    data = self.get_data()
-    if data is None:
-      return None
-    self.values = data[0,:,:,:,:]
-    return self.values
-
-  def add_to_mesh(self) -> None:
-    if self.values is None:
-      values = self.get_values()
-    if values is None:
-      print(f"Cannot add field '{self.name}' to mesh because its values could not be retrieved.")
-      return
-    self.mesh.add_field(self.name, values)
-    return
-
-  def reset(self) -> None:
-    if self.name in self.mesh.point_data:
-      self.mesh.point_data.pop(self.name)
-    if self.name in self.mesh.cell_data:
-      self.mesh.cell_data.pop(self.name)
-    self.values = None
-    return
-
-class DerivedField(Field):
-  def __init__(self, name:str, io_utils:IOutils, mesh:spv.YinYangMesh):
-    super().__init__(name, io_utils, mesh)
-    return
-  
-  def add_to_mesh(self) -> None:
-    if self.values is None:
-      values = self.get_values()
-    if values is None:
-      print(f"Cannot add field '{self.name}' to mesh because its values could not be retrieved.")
-      return
-    self.mesh[self.name] = values
-    return
-
-class Velocity(Field):
-  def __init__(self, name:str, io_utils:IOutils, mesh:spv.YinYangMesh):
-    super().__init__(name, io_utils, mesh)
-    return
-  
-  def get_values(self) -> np.ndarray|None:
-    if self.values is not None:
-      return self.values
-    data = self.get_data()
-    if data is None:
-      return None
-    values = data[0:3,:,:,:,:]
-    self.mesh.reconstruct_velocity(values)
-    self.values = values
-    return self.values
-  
-class Pressure(Field):
-  def __init__(self, name:str, io_utils:IOutils, mesh:spv.YinYangMesh):
-    super().__init__(name, io_utils, mesh)
-    return
-  
-  def get_values(self) -> np.ndarray|None:
-    if self.values is not None:
-      return self.values
-    data = self.get_data()
-    if data is None:
-      return None
-    self.values = data[3,:,:,:,:]
-    return self.values
-
-class SphericalField(DerivedField):
-  def __init__(self, name:str, io_utils:IOutils, mesh:spv.YinYangMesh, cartesian_field:Field):
-    super().__init__(name, io_utils, mesh)
-    self.field_x = cartesian_field
-    return
-  
-  def get_data(self) -> np.ndarray|None:
-    if self.field_x.name in self.mesh.cell_data or self.field_x.name in self.mesh.point_data:
-      return self.mesh[self.field_x.name]
-    else:
-      self.field_x.add_to_mesh()
-      return self.mesh[self.field_x.name]
-
-  def get_values(self) -> np.ndarray|None:
-    if self.values is not None:
-      return self.values
-    cartesian_vec = self.get_data()
-    if cartesian_vec is None:
-      return None
-    self.values = self.mesh.vector_cartesian_to_spherical(cartesian_vec)
-    return self.values
-
-class CartesianGradient(DerivedField):
-  def __init__(self, name:str, io_utils:IOutils, mesh:spv.YinYangMesh, field:Field):
-    super().__init__(name, io_utils, mesh)
-    self.mesh  = mesh
-    self.field = field
-    return
-  
-  def get_data(self) -> np.ndarray|None:
-    if self.field.name not in self.mesh.point_data:
-      phi = self.field.get_values()
-      if phi is None:
-        print(f"Cannot compute gradient for field '{self.field.name}' because its values could not be retrieved.")
-        return None
-    else:
-      phi = self.mesh.point_data[self.field.name]
-    return phi
-  
-  def get_values(self) -> np.ndarray|None:
-    if self.values is not None:
-      return self.values
-    field = self.get_data()
-    if field is None:
-      return None
-    if not self.mesh.is_point_field(field):
-      raise ValueError(f"Cannot compute gradient for field of shape {field.shape} that is not a point field ({self.mesh.number_of_points}).")
-    bs = field.shape[1] if field.ndim == 2 else 1
-    if bs == 1:
-      values = self.mesh.compute_gradient(field)
-    else:
-      values = np.zeros((self.mesh.number_of_cells, 3, bs), dtype=np.float64)
-      for b in range(bs):
-        values[:,:,b] = self.mesh.compute_gradient(field[:,b])
-    self.values = values
-    return self.values
-
-class SphericalGradient(DerivedField):
-  def __init__(self, name:str, io_utils:IOutils, mesh:spv.YinYangMesh, cartesian_gradient:CartesianGradient):
-    super().__init__(name, io_utils, mesh)
-    self.mesh = mesh
-    self.grad_x = cartesian_gradient
-    return
-  
-  def get_data(self):
-    if self.grad_x.name in self.mesh.cell_data or self.grad_x.name in self.mesh.point_data:
-      return self.mesh[self.grad_x.name]
-    else:
-      grad_x = self.grad_x.get_values()
-      if grad_x is None:
-        print(f"Cannot compute spherical gradient because Cartesian gradient values could not be retrieved.")
-        return None
-      return grad_x
-
-  def get_values(self) -> np.ndarray|None:
-    if self.values is not None:
-      return self.values
-    cartesian_gradient = self.get_data()
-    if cartesian_gradient is None:
-      return None
-    if cartesian_gradient.ndim == 2: # vector field
-      values = self.mesh.spherical_gradient(cartesian_gradient)
-    elif cartesian_gradient.ndim == 3: # tensor field
-      bs = cartesian_gradient.shape[2]
-      values = np.zeros((self.mesh.number_of_cells, 3, bs), dtype=np.float64)
-      for b in range(bs):
-        values[:,:,b] = self.mesh.spherical_gradient(cartesian_gradient[:,b])
-    self.values = values
-    return self.values
-
-def class_instances(mesh:spv.YinYangMesh, io_utils:IOutils) -> dict[str,Field]:
-  """
-  Dictionnary containing all the available field instances that can be added to the mesh.
-  Add here any field that can be computed from the available data.
-  
-  :param mesh: Volume mesh reconstructed as an unstructured grid
-  :type mesh: spv.YinYangMesh
-  :param io_utils: Path and file management utilities
-  :type io_utils: IOutils
-  :return: Dictionnary of field instances that can be added to the mesh, with field names as keys
-  :rtype: dict[str, Field]
-  """
-  avail_fields = {}
-  avail_fields["velocity"]    = Velocity("velocity", io_utils, mesh)
-  avail_fields["velocity_r"]  = SphericalField("velocity_r", io_utils, mesh, avail_fields["velocity"])
-  avail_fields["temperature"] = Field("temperature", io_utils, mesh)
-  avail_fields["grad_T"]      = CartesianGradient("grad_T", io_utils, mesh, avail_fields["temperature"])
-  avail_fields["grad_T_r"]    = SphericalGradient("grad_T_r", io_utils, mesh, avail_fields["grad_T"])
-  avail_fields["divergence"]  = Field("divergence", io_utils, mesh)
-  avail_fields["e2"]          = Field("e2", io_utils, mesh)
-  avail_fields["stress"]      = Field("stress", io_utils, mesh)
-  avail_fields["pressure"]    = Pressure("pressure", io_utils, mesh)
-  avail_fields["nrc"]         = Field("nrc", io_utils, mesh) # don't know what is this field
-  avail_fields["composition"] = Field("composition", io_utils, mesh)
-  avail_fields["primordial"]  = Field("primordial", io_utils, mesh)
-  avail_fields["proterozoic"] = Field("proterozoic", io_utils, mesh)
-  avail_fields["vorticity"]   = Field("vorticity", io_utils, mesh)
-  avail_fields["viscosity"]   = Field("viscosity", io_utils, mesh)
-  return avail_fields
-
-def add_fields_to_mesh(mesh:spv.YinYangMesh, fields_to_add:list[str], class_fields:dict[str,Field], regions:list[str]=["composition"]) -> None:
+def add_fields_to_mesh(mesh:spv.YinYangMesh, fields_to_add:list[str], class_fields:dict[str,spv.StagField], regions:list[str]=["composition"]) -> None:
   for field in fields_to_add:
     if field == "regions":
       continue # regions require special processing
@@ -376,7 +37,7 @@ def add_fields_to_mesh(mesh:spv.YinYangMesh, fields_to_add:list[str], class_fiel
       mesh.cell_data.pop(field)
   return
 
-def write_mesh(mesh:spv.YinYangMesh|spv.ShellMesh, io_utils:IOutils, outfname:str) -> None:
+def write_mesh(mesh:spv.YinYangMesh|spv.ShellMesh, io_utils:spv.IOutils, outfname:str) -> None:
   t0 = perf_counter()
   print(f"\tWriting mesh: {outfname}")
   mesh.save(os.path.join(io_utils.output_dir,outfname))
@@ -384,7 +45,7 @@ def write_mesh(mesh:spv.YinYangMesh|spv.ShellMesh, io_utils:IOutils, outfname:st
   print(f"\tMesh written in {t1-t0:g} seconds.")
   return
 
-def write_step(mesh:spv.YinYangMesh, io_utils:IOutils, class_fields:dict[str,Field]) -> None:
+def write_step(mesh:spv.YinYangMesh, io_utils:spv.IOutils, class_fields:dict[str,spv.StagField]) -> None:
   is_surface = io_utils.is_surface
   regions_list = io_utils.regions
   fields_to_add = io_utils.output_fields
@@ -409,12 +70,12 @@ def write_step(mesh:spv.YinYangMesh, io_utils:IOutils, class_fields:dict[str,Fie
     write_mesh(mesh, io_utils, outfname)
   return
 
-def clean_up(class_instances:dict[str,Field]) -> None:
+def clean_up(class_instances:dict[str,spv.StagField]) -> None:
   for field in class_instances:
     class_instances[field].reset()
   return
 
-def process_full_directory(mesh:spv.YinYangMesh, io_utils:IOutils, class_fields:dict[str,Field]) -> None:
+def process_full_directory(mesh:spv.YinYangMesh, io_utils:spv.IOutils, class_fields:dict[str,spv.StagField]) -> None:
   """
   Process the entire directory of output files.
   This function assumes that the steps are sequential and there are no missing steps in the directory.
@@ -441,7 +102,7 @@ def process_full_directory(mesh:spv.YinYangMesh, io_utils:IOutils, class_fields:
       io_utils.step += 1
   return
 
-def process_step_range(mesh:spv.YinYangMesh, io_utils:IOutils, class_fields:dict[str,Field]) -> None:
+def process_step_range(mesh:spv.YinYangMesh, io_utils:spv.IOutils, class_fields:dict[str,spv.StagField]) -> None:
   """
   Process a specified range of steps, defined by io_utils.step_start and io_utils.step_end.
   This function is more robust to missing steps in the directory, but requires the user to specify the step range and dstep.
@@ -466,7 +127,7 @@ def process_step_range(mesh:spv.YinYangMesh, io_utils:IOutils, class_fields:dict
       io_utils.timeseries["step"].append(str(step).zfill(5))
   return
 
-def process_model(iou:IOutils) -> None:
+def process_model(iou:spv.IOutils) -> None:
   if iou.step is None:
     iou.step = iou.step_start
     if iou.step_end is None:
@@ -481,7 +142,8 @@ def process_model(iou:IOutils) -> None:
   print(f"Generating volume mesh using file {fname}...")
   mesh = spv.YinYangMesh(os.path.join(iou.model_dir,fname))
   # create the available class instances for the fields that can be added to the mesh
-  class_fields = class_instances(mesh, iou)
+  #class_fields = class_instances(mesh, iou)
+  class_fields = spv.fields_instances(iou, mesh)
 
   match use_case:
     case "single_step":
@@ -618,7 +280,7 @@ def main():
       start_step = int(timeseries["step"][-1]) + 1
       print(f"Updated start step to {start_step} based on existing pvd file.")
 
-  io_utils = IOutils(
+  io_utils = spv.IOutils(
     model_name=paths["model"],
     model_dir=paths["directory"],
     basedir=paths["base"]["model"],
@@ -663,7 +325,7 @@ def test():
       start_step = int(timeseries["step"][-1]) + 1
       print(f"Updated start step to {start_step} based on existing pvd file.")
 
-  io_utils = IOutils(
+  io_utils = spv.IOutils(
     model_name=model,
     model_dir=mdir,
     basedir=basedir,
@@ -705,7 +367,7 @@ def test2():
   #  start_step = int(timeseries["step"][-1]) + 1
   #  print(f"Updated start step to {start_step} based on existing pvd file.")
 
-  io_utils = IOutils(
+  io_utils = spv.IOutils(
     model_name=model,
     model_dir=mdir,
     basedir=basedir,
@@ -725,7 +387,7 @@ def test2():
   print("Generating volume mesh...")
   mesh = spv.YinYangMesh(os.path.join(io_utils.model_dir,fname))
   # create the available class instances for the fields that can be added to the mesh
-  class_fields = class_instances(mesh, io_utils)
+  class_fields = spv.fields_instances(io_utils, mesh)
   
   #found = True
   #io_utils.step = start_step
